71f7888c4670a600c584b6ac2a88b28ee563dc3b
Qluqlu3 <plksmcgof@gmail.com>
2025-10-09 02:06:08 +0900

    Fix: eslint.config, eslint script and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,43 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginImport from 'eslint-plugin-import';
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    plugins: {import: pluginImport, "simple-import-sort": simpleImportSort,},
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react', '^next'],
+            ['^@?\\w'],
+            ['^@/components'],
+            ['^@/'],
+            ['^\\.'],
+            ['^.+\\.css$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+    },
   },
-];
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
